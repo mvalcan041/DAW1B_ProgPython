20d0ffb9917b64b67b4cd7118ea293210ea2758c
Canexy <mariocanalejas@hotmail.com>
2023-10-12 19:24:10 +0200

    Changes 4.0

--- ejercicios_u1/e1-30.py
+++ ejercicios_u1/e1-30.py
@@ -8,49 +8,53 @@
 
 """
 Inicio
-    Escribe "Dime un número: "
+    Escribe "Dime un número inicial: "
     Lee num
-    Escribe "Dime un número de incremento: "
+    Escribe "Dime un número incremental: "
     Lee inc
-    Escribe "Dime un número como total de la serie: "
-    Lee serie
+    Escribe "Dime el total de números en la serie: "
+    Lee ser
 
-    Mientras (inc < 0) or (serie < 0) hacer
-        Escribe "Error, debe introducir valores mayores a 0 tanto en incremento como en serie."
-        Escribe "Dime otro número de incremento: "
+    Mientras (inc <= 0) hacer
+        Escribe "Error, el número incremental debe ser mayor que 0. Introduza un número de nuevo: "
         Lee inc
-        Escribe "Dime otro número como total de la serie: "
-        Lee serie
 
-    Escribe "SERIE =>" + num + "-"
-    cont = num
-    Mientras (cont != (serie - 1) hacer
-        Escribe num + "..."
-        num += inc
+    Mientras (ser <= 0) hacer
+        Escribe "Error, el total de números en la serie debe ser mayor que 0. Introduza un número de nuevo: "
+        Lee ser
     
-    Escribe (num + inc) + "-"
-    Escribe serie
+    cont = 0
+    Escribe "Serie => " + ini + "-"
+
+    Mientras (cont < (ser - 3)) hacer
+        ini += inc
+        Escribir ini + ".."
+        cont += 1
+
+    ini += inc
+    Escribir ini + "-" + (ini + inc)
 Fin
 """
 
 # Python
 
-num = int(input(f"Dime un número:\n"))
-inc = int(input(f"Dime un número de incremento:\n"))
-serie = int(input(f"Dime un número como total de la serie:\n"))
+ini = int(input(f"Dime un número inicial:\n"))
+inc = int(input(f"Dime un número incremental:\n"))
+ser = int(input(f"Dime el total de números en la serie:\n"))
 
-while (inc < 0) or (serie < 0):
-    print(f"Error, debe introducir valores mayores a 0 tanto en incremento como en serie.")
-    inc = int(input(f"Dime otro número de incremento:\n"))
-    serie = int(input(f"Dime otro número como total de la serie:\n"))
+while (inc <= 0):
+    inc = int(input(f"Error, el número incremental debe ser mayor que 0. Introduza un número de nuevo:\n"))
+    
+while (ser <= 0):
+    ser = int(input(f"Error, el total de números en la serie debe ser mayor que 0. Introduza un número de nuevo:\n"))
 
-print(f"SERIE => {num}-")
+cont = 0
+print(f"Serie => {ini}-", end= "")
 
-cont = num
-while (cont != (serie - 1)):
-    print(f"{num}...")
-    num += inc
+while cont < (ser - 3):
+    ini += inc
+    print(f"{ini}..", end= "")
+    cont += 1
 
-print(f"{num + inc}-{serie}")
-
-# Hola amiguis 2
+ini += inc
+print(f"{ini}-{ini + inc}")
