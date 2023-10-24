3e19e6a51592b3acd93964c67bb55db09b0ab02f
Canexy <mariocanalejas@hotmail.com>
2023-10-24 20:42:47 +0200

    Added: Práctica 2.1; e2_1_1.py * Changes *

---
+++ unidad_2/ejercicios_u2/práctica_2.1/test_e2_1_1.py
@@ -0,0 +1,24 @@
+
+# Pytest "Ejercicio 2.1.1"
+
+from e2_1_1 import edad
+import pytest
+
+def test_comparacion():
+    assert edad(18) == True
+    assert edad(2) == False
+    assert edad(72) == True
+
+@pytest.mark.parametrize(
+    "input_n1, expected",
+    [
+      (1, False),
+      (-1, False),
+      (100, True),
+      (14, False),
+      (54, True),
+      (9, False)
+    ]
+  )
+def test_edad_params(input_n1, expected):
+    assert edad(input_n1) == expected
