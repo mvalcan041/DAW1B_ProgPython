6f0cbd88489427cd7aa08599293f76cca46132f9
mvalcan041 <mvalcan041@g.educaand.es>
2023-10-30 18:19:18 +0100

    Changes *

--- pruebas/test_prueba2.py
+++
@@ -1,8 +0,0 @@
-
-from prueba2 import comparacion
-import pytest
-
-def test_comparacion():
-    assert comparacion(1, 1) == 0
-    assert comparacion(1, 0) == 1
-    assert comparacion(-100, 100) == 100
